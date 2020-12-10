f17eec087999f808323b64d5aacc205f17008913
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-12-10 17:06:51 +1100

    Added a few more methods

--- WordClass.php
+++ WordClass.php
@@ -1,7 +1,6 @@
 <?php
    // Ref - https://www.w3schools.com/php/php_ref_string.asp
-   // A basic class with getters and setters
-   class Word {
+    class Word {
       /* Member variables */
       var $word;
 
@@ -15,19 +14,37 @@
 
       function chop(){
          return chop($this->word, "World!")    ."<br/>";
-     }
+      }
      
-     function countChars(){
+      function countChars(){
       return count_chars($this->word, 3)    ."<br/>";
-   }
+      }
 
-   function encString(){
+      function encString(){
       return sha1($this->word)    ."<br/>";
-   }
+      }
+
+      function length(){
+         return strlen($this->word)    ."<br/>";
+      }
+
+      function toUpper(){
+         return strtoupper($this->word)."<br/>";
+      }
+
+      function trim(){
+         return trim($this->word)."<br/>";
+      }
       
    }
+
+
    $word1 = new Word("Hello World");
    echo $word1->addSlashes();
    echo $word1->chop();
    echo $word1->countChars();
    echo $word1->encString();
+   echo $word1->length();
+   echo $word1->toUpper();
+   echo $word1->trim();
+   
