bb634c1e2015a7b4e65a292f67eb247fb386bfde
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-12-16 10:58:37 +1100

    Extended in the sort associate arrays file with descending example using ksort

--- Arrays/SortAssociateArray.php
+++ Arrays/SortAssociateArray.php
@@ -1,6 +1,6 @@
 <?php
 
-
+// Example 1
 $ceu = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg",
 "Belgium"=> "Brussels", "Denmark"=>"Copenhagen",
 "Finland"=>"Helsinki", "France" => "Paris",
@@ -23,5 +23,50 @@ foreach($ceu as $country => $capital)
 echo "The capital of $country is $capital"."\n" . "<br>" ;
 }
 
+// EXAMPLE 2
 
-?>
+echo "Associative array : Ascending order sort by value";
+
+$array2=array("Sophia"=>"31","Jacob"=>"41","William"=>"39","Ramesh"=>"40"); 
+
+asort($array2);
+
+foreach($array2 as $y=>$y_value)
+{
+echo "Age of ".$y." is : ".$y_value."
+";
+}
+
+echo "Associative array : Ascending order sort by Key";
+
+$array3=array("Sophia"=>"31","Jacob"=>"41","William"=>"39","Ramesh"=>"40"); 
+ksort($array3);
+
+foreach($array3 as $y=>$y_value)
+{
+echo "Age of ".$y." is : ".$y_value."
+";
+}
+
+echo "Associative array : Descending order sorting by Value";
+
+$age=array("Sophia"=>"31","Jacob"=>"41","William"=>"39","Ramesh"=>"40");
+
+arsort($age);
+
+foreach($age as $y=>$y_value)
+{
+echo "Age of ".$y." is : ".$y_value."
+";
+}
+
+echo "Associative array : Descending order sorting by Key";
+
+$array4=array("Sophia"=>"31","Jacob"=>"41","William"=>"39","Ramesh"=>"40"); 
+krsort($array4);
+
+foreach($array4 as $y=>$y_value)
+{
+echo "Age of ".$y." is : ".$y_value."
+";
+}
