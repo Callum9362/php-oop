f76c20e99d6a3396f41a96b24a7ecbb96fb9c1f5
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-12-15 17:12:12 +1100

    Added in the color exercise for arrays

---
+++ Arrays/ColorExercise.php
@@ -0,0 +1,7 @@
+<?php
+
+$fruits = array ('white', 'green', 'red');
+
+echo "The memory of that scene for me is like a frame of film forever frozen at that moment: the ".$fruits[2]." carpet, the ".$fruits[1]." lawn, the ".$fruits[0]." house, the leaden sky. The new president and his first lady. - Richard M. Nixon"
+
+?>
